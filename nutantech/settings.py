bf9312b43ba38cf8699f9f29b966fc526f861a0f
shailesh <shailesh.kumarcs@gmail.com>
2018-05-12 13:22:36 +0530

    add home page template

--- nutantech/settings.py
+++ nutantech/settings.py
@@ -129,3 +129,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_in_pro","static_root")
+#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env","static_root")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static_in_pro","our_static"),
+    #os.path.join(BASE_DIR, "static_in_env"),
+    #'/var/www/static/',
+)
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT =os.path.join(BASE_DIR, "static_in_pro","media_root")
